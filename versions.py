22e4b40207570e5b3f55ad5f7cd988fcf80cd92b
Ryan <ryankwong478@gmail.com>
2023-06-13 20:54:32 -0700

    added library versions in versions.py

--- versions.py
+++ versions.py
@@ -1,10 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from pandas_datareader import data as pdr
-from keras.models import load_model
 import streamlit as st
-from sklearn.preprocessing import MinMaxScaler
 import yfinance as yfin
 
 print("NumPy: " + np.__version__)
